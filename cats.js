524a5de5d981614f19afee594281ea278960377b
TianYiyun <LiZexing@Zers-Mac.local>
2018-09-15 18:27:35 +0800

    added cat like function

---
+++ cats.js
@@ -0,0 +1,10 @@
+//test about lab 1... follow Chris's instructions
+let cats = [
+    {name: 'july', likes:'sleep'},
+    {name: 'sweet', likes:'eat'}
+
+]
+for (let i=0;i<cats.length; i++) {
+    console.log(` ${cats[i].name} likes   ${cats[i].likes}`);
+
+}
